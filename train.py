cfe44789fd62b17d6a2b9ce1b3a91314dc7bd40e
Aimé T Shangula <31923592+aimazin@users.noreply.github.com>
2020-10-31 21:54:24 +0000

    Add files via upload

--- train.py
+++ train.py
@@ -1,3 +1,9 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
 from sklearn.linear_model import LogisticRegression
 import argparse
 import os
@@ -9,20 +15,32 @@ from sklearn.preprocessing import OneHotEncoder
 import pandas as pd
 from azureml.core.run import Run
 from azureml.data.dataset_factory import TabularDatasetFactory
+from azureml.core import Workspace
+from azureml.core import Dataset
+
 
 # TODO: Create TabularDataset using TabularDatasetFactory
 # Data is located at:
 # "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
 
-ds = ### YOUR CODE HERE ###
+# In[ ]:
 
-x, y = clean_data(ds)
+
+ds = TabularDatasetFactory.from_delimited_files('https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv')
+
 
 # TODO: Split data into train and test sets.
 
-### YOUR CODE HERE ###a
+# ## YOUR CODE HERE ###a
+
+# In[ ]:
+
 
 run = Run.get_context()
+
+
+# In[ ]:
+
 
 def clean_data(data):
     # Dict for cleaning data
@@ -47,26 +65,91 @@ def clean_data(data):
     x_df["month"] = x_df.month.map(months)
     x_df["day_of_week"] = x_df.day_of_week.map(weekdays)
     x_df["poutcome"] = x_df.poutcome.apply(lambda s: 1 if s == "success" else 0)
+    return (x_df)
 
-    y_df = x_df.pop("y").apply(lambda s: 1 if s == "yes" else 0)
-    
+
+# - x, y = clean_data(ds)
+# - x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=2020)
+
+# In[ ]:
+
 
 def main():
     # Add arguments to script
+    
+    
+    
+     def clean_data(data):
+    # Dict for cleaning data
+        months = {"jan":1, "feb":2, "mar":3, "apr":4, "may":5, "jun":6, "jul":7, "aug":8, "sep":9, "oct":10, "nov":11, "dec":12}
+        weekdays = {"mon":1, "tue":2, "wed":3, "thu":4, "fri":5, "sat":6, "sun":7}
+
+        # Clean and one hot encode data
+        x_df = data.to_pandas_dataframe().dropna()
+        jobs = pd.get_dummies(x_df.job, prefix="job")
+        x_df.drop("job", inplace=True, axis=1)
+        x_df = x_df.join(jobs)
+        x_df["marital"] = x_df.marital.apply(lambda s: 1 if s == "married" else 0)
+        x_df["default"] = x_df.default.apply(lambda s: 1 if s == "yes" else 0)
+        x_df["housing"] = x_df.housing.apply(lambda s: 1 if s == "yes" else 0)
+        x_df["loan"] = x_df.loan.apply(lambda s: 1 if s == "yes" else 0)
+        contact = pd.get_dummies(x_df.contact, prefix="contact")
+        x_df.drop("contact", inplace=True, axis=1)
+        x_df = x_df.join(contact)
+        education = pd.get_dummies(x_df.education, prefix="education")
+        x_df.drop("education", inplace=True, axis=1)
+        x_df = x_df.join(education)
+        x_df["month"] = x_df.month.map(months)
+        x_df["day_of_week"] = x_df.day_of_week.map(weekdays)
+        x_df["poutcome"] = x_df.poutcome.apply(lambda s: 1 if s == "success" else 0)
+
+        y_df = x_df.pop("y").apply(lambda s: 1 if s == "yes" else 0)
+        return x_df, y_df
+
+
+
+    run = Run.get_context()
+
+
+
     parser = argparse.ArgumentParser()
-
-    parser.add_argument('--C', type=float, default=1.0, help="Inverse of regularization strength. Smaller values cause stronger regularization")
-    parser.add_argument('--max_iter', type=int, default=100, help="Maximum number of iterations to converge")
-
+    parser.add_argument('--C',type=float, default=1.0, help="Inverse of regularization strength. Smaller values cause stronger regularization")
+    parser.add_argument('--max_iter',type=int, default=100, help="Maximum number of iterations to converge")
     args = parser.parse_args()
+
+
+
+
+    ds = run.input_datasets['Bank_Data']
+
+
+
+    x, y = clean_data(ds)
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=2020)
+
+
 
     run.log("Regularization Strength:", np.float(args.C))
     run.log("Max iterations:", np.int(args.max_iter))
 
-    model = LogisticRegression(C=args.C, max_iter=args.max_iter).fit(x_train, y_train)
+    model = LogisticRegression(C=args.C, max_iter=args.max_iter,solver="liblinear").fit(x_train, y_train)
 
     accuracy = model.score(x_test, y_test)
     run.log("Accuracy", np.float(accuracy))
 
+    joblib.dump(value=model, filename='outputs/hyptun_model.joblib')
+
+
+
+    run.complete()
+    
+    
+    
+
+
+# In[ ]:
+
+
 if __name__ == '__main__':
     main()
+
